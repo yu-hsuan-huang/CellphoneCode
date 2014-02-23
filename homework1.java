1679b30b8b47af83505218683c6a7d95e54528db
yu-hsuan-huang <a88809748@gmail.com>
2014-02-23 23:09:20 +0800

    行動裝置

---
+++ homework1.java
@@ -0,0 +1,7 @@
+public class homework1 {
+  public static void main(String args[])  { 
+  
+	System.out.println("<九月九日憶山東兄弟>\n作者：王維\n");
+	System.out.println("獨在异鄉為异客，每逢佳節倍思親。\n遙知兄弟登高處，遍插茱萸少一人。\n");
+  }
+}
